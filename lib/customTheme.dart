f3ac0883fc201e3eb2ef2f6c0c2fc8788228d0d1
Viachaslau Lyskouski <deus@creativity.by>
2023-06-17 12:54:06 +0200

    [#2] [NF] Create main window. Update fonts

--- lib/customTheme.dart
+++ lib/customTheme.dart
@@ -1,4 +1,7 @@
 import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomTheme {
   const CustomTheme({
@@ -7,11 +10,33 @@ class CustomTheme {
 
   final AdaptiveWindowType windowType;
 
-  bool isLower(AdaptiveWindowType size) {
-    return windowType <= size;
-  }
+  bool isLower(AdaptiveWindowType size) => windowType <= size;
 
-  double getIndent() {
-    return isLower(AdaptiveWindowType.small) ? 8.0 : 16.0;
+  double getIndent() => isLower(AdaptiveWindowType.small) ? 8.0 : 16.0;
+
+  double letterSpacingOrNone(double letterSpacing) => kIsWeb ? 0.0 : letterSpacing;
+
+  TextTheme getFonts(TextTheme base) {
+    return base.copyWith(
+      bodyMedium: GoogleFonts.robotoCondensed(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        letterSpacing: letterSpacingOrNone(0.5),
+      ),
+      bodyLarge: GoogleFonts.eczar(
+        fontSize: 40,
+        fontWeight: FontWeight.w400,
+        letterSpacing: letterSpacingOrNone(1.4),
+      ),
+      labelLarge: GoogleFonts.robotoCondensed(
+        fontWeight: FontWeight.w700,
+        letterSpacing: letterSpacingOrNone(2.8),
+      ),
+      headlineSmall: GoogleFonts.eczar(
+        fontSize: 40,
+        fontWeight: FontWeight.w600,
+        letterSpacing: letterSpacingOrNone(1.4),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
+import 'package:app_finance/customTheme.dart';
 import 'package:app_finance/routes.dart' as routes;
 import 'package:app_finance/routes/homePage.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +14,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var theme = CustomTheme(windowType: getWindowType(context));
     return MaterialApp(
       title: 'terCAD: Finance',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
+        textTheme: theme.getFonts(ThemeData.light().textTheme),
         useMaterial3: true,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
+        textTheme: theme.getFonts(ThemeData.dark().textTheme),
         useMaterial3: true,
       ),
       themeMode: ThemeMode.system,
