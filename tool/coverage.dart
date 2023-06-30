2f44ec839ee775e916218211ffc09ad0a2515bc0
Viachaslau Lyskouski <deus@creativity.by>
2023-06-30 20:59:13 +0200

    [#44] [BP] Unit Tests automation. Improve coverage, doc update

---
+++ tool/coverage.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void scanDirectory(Directory root, Directory directory, List<String> files) {
+  directory.listSync(recursive: true).forEach((entity) {
+    if (entity is File &&
+        entity.path.endsWith('.dart') &&
+        !entity.path.endsWith('.g.dart')) {
+      files.add(entity.absolute.path
+          .replaceAll(root.absolute.path, '')
+          .replaceAll('\\', '/'));
+    } else if (entity is Directory) {
+      scanDirectory(root, entity, files);
+    }
+  });
+}
