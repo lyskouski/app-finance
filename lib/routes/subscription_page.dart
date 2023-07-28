4106a97aed0df62d7ee590f3ca9cafabb093e4c8
Viachaslau Lyskouski <deus@creativity.by>
2023-07-28 23:36:42 +0200

    [#20] [NF] Add Subscriptions. Fix platform verification

--- lib/routes/subscription_page.dart
+++ lib/routes/subscription_page.dart
@@ -2,13 +2,12 @@
 // Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
-
 import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
 import 'package:app_finance/helpers/theme_helper.dart';
 import 'package:app_finance/routes/abstract_page.dart';
 import 'package:app_finance/widgets/subscription/other_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 
 class SubscriptionPage extends AbstractPage {
@@ -34,7 +33,8 @@ class SubscriptionPageState extends AbstractPageState<SubscriptionPage> {
         children: [
           Text(AppLocalizations.of(context)!.subscription),
           const Divider(),
-          Platform.isIOS || Platform.isMacOS
+          defaultTargetPlatform == TargetPlatform.iOS ||
+                  defaultTargetPlatform == TargetPlatform.macOS
               ? const SizedBox()
               : const OtherWidget(),
         ],
