ca538c0754b900023bb438f0270da957d72ee921
Viachaslau Lyskouski <deus@creativity.by>
2023-08-13 08:30:20 +0200

    [#130] [BP] Benchmarking Tool. Check provider

---
+++ test/unit/_classes/currency/currency_provider_test.dart
@@ -0,0 +1,25 @@
+import 'package:app_finance/_classes/currency/currency_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CurrencyProvider', () {
+    group('findByCode', () {
+      final testCases = [
+        (code: null, result: null),
+        (code: 'USD', result: 'USD'),
+        (code: 'ABC', result: null),
+      ];
+
+      for (var v in testCases) {
+        test('$v', () {
+          expect(CurrencyProvider.findByCode(v.code)?.code, v.result);
+        });
+      }
+    });
+
+    test('getAll', () {
+      final result = CurrencyProvider.getAll();
+      expect(result.isNotEmpty, true);
+    });
+  });
+}
