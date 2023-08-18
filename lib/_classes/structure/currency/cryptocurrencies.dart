5fba0141fdf7dc6e5747ec2ac3e99ed8717dd5e0
Viachaslau Lyskouski <deus@creativity.by>
2023-08-18 12:57:29 +0200

    [#159] [RF] SOLID Refactoring. Update CC to 120

--- lib/_classes/structure/currency/cryptocurrencies.dart
+++ lib/_classes/structure/currency/cryptocurrencies.dart
@@ -1,6 +1,5 @@
 // Copyright 2023 The terCAD team. All rights reserved.
-// Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be
-// found in the LICENSE file.
+// Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
 
 // Cryptocurrencies: https://en.wikipedia.org/wiki/List_of_cryptocurrencies
 List<List<String>> cryptoList = [
