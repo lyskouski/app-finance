7ac0a44c9103102a99e0f5507d2d246c900f6a04
Viachaslau Lyskouski <deus@creativity.by>
2023-08-21 09:51:15 +0200

    [#146] [RF] Improve Forecast Chart. Localize test data

--- test/pump_main.dart
+++ test/pump_main.dart
@@ -1,6 +1,8 @@
 // Copyright 2023 The terCAD team. All rights reserved.
 // Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:app_finance/_classes/storage/app_data.dart';
 import 'package:app_finance/_classes/herald/app_locale.dart';
 import 'package:app_finance/_classes/herald/app_theme.dart';
@@ -8,6 +10,7 @@ import 'package:app_finance/_classes/gen/generate_with_method_setters.dart';
 import 'package:app_finance/_mixins/shared_preferences_mixin.dart';
 import 'package:app_finance/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:provider/provider.dart';
@@ -21,8 +24,11 @@ import 'pump_main.mocks.dart';
 import 'pump_main.wrapper.dart';
 
 class PumpMain {
+  static const path = './coverage/data';
+
   static init(WidgetTester tester, [bool isIntegration = false]) async {
     final pumpMain = PumpMain();
+    wrapProvider(tester, 'plugins.flutter.io/path_provider', '$path/${UniqueKey()}');
     await pumpMain.initPref(isIntegration);
     await pumpMain.initMain(tester, isIntegration);
   }
@@ -33,6 +39,20 @@ class PumpMain {
       appData.isLoading = false;
     }
     return appData;
+  }
+
+  static void cleanUpData() {
+    final dir = Directory(path);
+    if (dir.existsSync()) {
+      dir.deleteSync(recursive: true);
+    }
+  }
+
+  static void wrapProvider(WidgetTester tester, String channel, String output) {
+    tester.binding.defaultBinaryMessenger.setMockMethodCallHandler(
+      MethodChannel(channel),
+      (MethodCall methodCall) => Future.value(output),
+    );
   }
 
   Future<void> initPref(bool isIntegration) async {
