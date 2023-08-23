451b86fca58b7241eabc66e025bb3d3e46169b1d
Viachaslau Lyskouski <deus@creativity.by>
2023-08-23 18:40:10 +0200

    [#179] [CR] Improve Charts. Improve forecast chart

--- lib/charts/painter/forecast_chart_painter.dart
+++ lib/charts/painter/forecast_chart_painter.dart
@@ -5,6 +5,7 @@ import 'package:app_finance/_classes/math/monte_carlo_simulation.dart';
 import 'package:app_finance/charts/interface/chart_data.dart';
 import 'package:app_finance/charts/painter/abstract_painter.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class ForecastChartPainter extends AbstractPainter {
   final List<ChartData> data;
@@ -25,9 +26,10 @@ class ForecastChartPainter extends AbstractPainter {
     }
     size = this.size ?? size;
     for (final scope in data) {
-      final total = _paint(canvas, scope.data, size, scope.color);
+      _paint(canvas, scope.data, size, scope.color);
       final dx = scope.data.last.dx;
-      if (scope.data.length > 2 && dx < xMax) {
+      final total = _sumY(scope.data);
+      if (scope.data.length > 2 && dx < xMax && total < yMax) {
         final cycles = (xMax - dx) ~/ usDay;
         final forecast = [scope.data.last];
         forecast.addAll(MonteCarloSimulation(cycles: cycles).generate(scope.data, usDay, xMax - 2 * usDay));
@@ -48,32 +50,25 @@ class ForecastChartPainter extends AbstractPainter {
   }
 
   Offset _getMedian(List<Offset> scope) {
-    return Offset(
-      (scope.last.dx + scope.first.dx) / 2,
-      _sumY(scope.sublist(0, scope.length ~/ 2)),
-    );
+    return Offset((scope.last.dx + scope.first.dx) / 2, _sumY(scope));
   }
 
-  double _paint(Canvas canvas, List<Offset> scope, Size size, Color color, [double total = 0.0]) {
+  void _paint(Canvas canvas, List<Offset> scope, Size size, Color color, [double total = 0.0]) {
     if (scope.isEmpty) {
-      return 0.0;
+      return;
     }
     Offset startPoint;
     [_, startPoint] = _bind(scope.first, size, total);
-    int i = 0;
-    Offset point;
-    [i, point] = _paintDots(canvas, scope, size, color, total);
-    int third = i ~/ 3;
-    if (third > 0) {
-      Offset startBezier = getValue(_getMedian(scope.sublist(0, third)), size, total);
-      total += _sumY(scope.sublist(0, third));
-      Offset middleBezier = getValue(_getMedian(scope.sublist(third, 2 * third)), size, total);
-      total += _sumY(scope.sublist(third, 2 * third));
-      Offset endBezier = getValue(_getMedian(scope.sublist(2 * third, i)), size, total);
-      _paintCurve(canvas, startPoint, startBezier, middleBezier, color);
-      _paintCurve(canvas, middleBezier, endBezier, point, color);
+    int count = 0;
+    Offset endPoint;
+    [count, endPoint] = _paintDots(canvas, scope, size, color, total);
+    int half = count ~/ 2;
+    if (half > 0) {
+      Offset startBezier = getValue(_getMedian(scope.sublist(0, half)), size, total);
+      total += _sumY(scope.sublist(0, half));
+      Offset endBezier = getValue(_getMedian(scope.sublist(half, count)), size, total);
+      _paintCurve(canvas, startPoint, startBezier, endBezier, endPoint, color);
     }
-    return total + point.dy;
   }
 
   List<dynamic> _paintDots(Canvas canvas, List<Offset> scope, Size size, Color color, double total) {
@@ -88,7 +83,7 @@ class ForecastChartPainter extends AbstractPainter {
       }
       _paintDot(canvas, point, color);
     }
-    return [i - 1, point];
+    return [i, point];
   }
 
   void _paintDot(Canvas canvas, Offset point, Color color) {
@@ -96,15 +91,13 @@ class ForecastChartPainter extends AbstractPainter {
     canvas.drawCircle(point, 2.2, dot);
   }
 
-  _paintCurve(Canvas canvas, Offset startPoint, Offset controlPoint, Offset endPoint, Color color) {
+  _paintCurve(Canvas canvas, Offset p0, Offset k1, Offset k2, Offset p1, Color color) {
     final line = Paint()
       ..color = color
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2;
-    final path = Path()
-      ..moveTo(startPoint.dx, startPoint.dy)
-      ..quadraticBezierTo(controlPoint.dx, controlPoint.dy, endPoint.dx, endPoint.dy);
-    // qubicTo
+    final path = Path()..moveTo(p0.dx, p0.dy);
+    path.cubicTo(k1.dx, k1.dy, k2.dx, k2.dy, p1.dx, p1.dy);
     canvas.drawPath(path, line);
   }
 }
