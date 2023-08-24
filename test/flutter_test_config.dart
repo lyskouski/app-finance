9fdb9a8aebb1bfc1e52a684129b90fbf28246f8b
Viachaslau Lyskouski <deus@creativity.by>
2023-08-24 22:17:02 +0200

    [#179] [CR] Improve Charts. Set assert to 2.1% of deviations

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -5,10 +5,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'e2e/_steps/screen_comparator.dart';
 
-/// Customize your threshold here
-/// For example, the error threshold here is 0.5%
-/// Golden tests will pass if the pixel difference is equal to or below 0.5%
-const _kGoldenTestsThreshold = 0.5 / 100;
+// Golden tests will pass if the pixel difference is equal to or below 2.1%
+const _kGoldenTestsThreshold = 2.1 / 100;
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   if (goldenFileComparator is LocalFileComparator) {
