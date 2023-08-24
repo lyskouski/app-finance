5867d52439bdb1f83f33e23ab54d85d8468b39f8
Viachaslau Lyskouski <deus@creativity.by>
2023-08-24 08:06:32 +0200

    [#179] [CR] Improve Charts. Add pie chart

---
+++ lib/charts/painter/pie_radius_painter.dart
@@ -0,0 +1,44 @@
+// Copyright 2023 The terCAD team. All rights reserved.
+// Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
+
+import 'package:app_finance/charts/interface/chart_value.dart';
+import 'package:app_finance/charts/painter/abstract_painter.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class PieRadiusPainter extends AbstractPainter {
+  final List<ChartValue> data;
+  late final double max;
+  double radius = 0;
+  Offset center = const Offset(0, 0);
+
+  PieRadiusPainter({
+    required this.data,
+    required super.indent,
+  }) {
+    max = data.fold(0.0, (v, o) => v + o.value) * (1 + indent * data.length);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    center = Offset(size.width, size.height * 3 / 4);
+    radius = size.width / 2;
+
+    double startPoint = pi / 2;
+    for (int i = 0; i < data.length; i++) {
+      startPoint = _drawArc(canvas, size, startPoint, i);
+    }
+  }
+
+  double _drawArc(Canvas canvas, Size size, double startPoint, int step) {
+    final shift = 7 * (step + 1);
+    final paint = Paint()
+      ..color = data[step].color
+      ..strokeWidth = size.width / 2 - shift
+      ..style = PaintingStyle.stroke;
+    const full = 2 * pi;
+    double endPoint = (data[step].value / max) * full;
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radius - shift / 2), startPoint, endPoint, false, paint);
+    return startPoint + endPoint + indent * full;
+  }
+}
--- lib/widgets/metrics/account_tab.dart
+++ lib/widgets/metrics/account_tab.dart
@@ -8,7 +8,10 @@ import 'package:app_finance/_classes/structure/account_app_data.dart';
 import 'package:app_finance/_classes/structure/currency/exchange.dart';
 import 'package:app_finance/_configs/theme_helper.dart';
 import 'package:app_finance/charts/gauge_chart.dart';
+import 'package:app_finance/charts/interface/chart_value.dart';
+import 'package:app_finance/charts/interface/ohlc_data.dart';
 import 'package:app_finance/charts/ohlc_chart.dart';
+import 'package:app_finance/charts/pie_radius_chart.dart';
 import 'package:app_finance/widgets/_wrappers/row_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -20,23 +23,46 @@ class AccountTab extends StatelessWidget {
     required this.store,
   });
 
+  List<OhlcData> _getData(List<AccountAppData> accounts, DateTime xMin) {
+    final exchange = Exchange(store: store);
+    return DataHandler.generateOhlcSummary(
+      store.getMultiLog(accounts),
+      exchange: exchange,
+      cut: xMin,
+    );
+  }
+
+  double _getHealthData(List<OhlcData> data) {
+    double health = 0;
+    if (data.isNotEmpty && data.first.high > 0) {
+      health = 100 * (data.last.close - data.first.high) / data.first.high;
+    }
+    return health;
+  }
+
+  List<ChartValue> _getCurrencyDistribution(List<AccountAppData> accounts) {
+    final exchange = Exchange(store: store);
+    final result = <String, ChartValue>{};
+    for (int i = 0; i < accounts.length; i++) {
+      final code = accounts[i].currency?.code ?? '?';
+      final value = exchange.reform(accounts[i].details, accounts[i].currency, Exchange.defaultCurrency);
+      if (!result.containsKey(code)) {
+        result[code] = ChartValue(0, color: accounts[i].color ?? Colors.grey, tooltip: code);
+      }
+      result[code]!.value += value;
+    }
+    return result.values.toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
     double indent = ThemeHelper.getIndent();
     double width = ThemeHelper.getWidth(context, 4);
-    final exchange = Exchange(store: store);
     final now = DateTime.now();
     final xMin = DateTime(now.year, now.month - 5);
-    final data = DataHandler.generateOhlcSummary(
-      store.getMultiLog(store.getList(AppDataType.accounts).cast<AccountAppData>()),
-      exchange: exchange,
-      cut: xMin,
-    );
-    double health = 0;
-    if (data.isNotEmpty && data.first.high > 0) {
-      health = 100 * (data.last.close - data.first.high) / data.first.high;
-    }
+    final accountList = store.getList(AppDataType.accounts).cast<AccountAppData>();
+    final data = _getData(accountList, xMin);
     final healthTxt = Text(
       AppLocale.labels.incomeHealth,
       style: Theme.of(context).textTheme.bodyMedium,
@@ -56,14 +82,14 @@ class AccountTab extends StatelessWidget {
               maxWidth: width - indent,
               chunk: [null, ThemeHelper.getTextWidth(healthTxt), 40 + indent],
               children: [
-                [],
+                const [SizedBox()],
                 [
                   healthTxt,
                   SizedBox(height: indent),
                 ],
                 [
                   GaugeChart(
-                    value: health,
+                    value: _getHealthData(data),
                     valueMin: 0,
                     valueMax: 100,
                     width: 40,
@@ -99,6 +125,13 @@ class AccountTab extends StatelessWidget {
                   overflow: TextOverflow.ellipsis,
                 ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: width * 0.5),
+              child: PieRadiusChart(
+                data: _getCurrencyDistribution(accountList),
+                width: width * 0.5,
+              ),
             ),
           ],
         ),
