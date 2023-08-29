8dabb5a8194317b43fb8f607ae2e98de60fc48e8
Viachaslau Lyskouski <deus@creativity.by>
2023-08-29 17:40:21 +0200

    [#189] [NF] Import QIF/OFX/HBCI Files. Assert date detection

---
+++ test/unit/_mixins/date_format_mixin_test.dart
@@ -0,0 +1,48 @@
+// Copyright 2023 The terCAD team. All rights reserved.
+// Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
+
+import 'package:app_finance/_classes/gen/generate_with_method_setters.dart';
+import 'package:app_finance/_mixins/date_format_mixin.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+@GenerateWithMethodSetters([DateFormatMixin])
+import 'date_format_mixin_test.wrapper.dart';
+
+void main() {
+  group('DateFormatMixin', () {
+    final object = WrapperDateFormatMixin();
+
+    group('splitDateTime', () {
+      final testCases = [
+        (date: 'Sun, 8/13/2023', result: ['Sun', ',', ' ', '8', '/', '13', '/', '2023']),
+        (date: '8/29/2023 12:00 PM', result: ['8', '/', '29', '/', '2023', ' ', '12', ':', '00', ' ', 'PM']),
+      ];
+
+      for (var v in testCases) {
+        test('$v', () {
+          expect(object.splitDateTime(v.date), v.result);
+        });
+      }
+    });
+
+    group('detectFormat', () {
+      final testCases = [
+        (date: ['Sun, 8/13/2023'], result: 'E, M/d/yyyy', locale: 'en'),
+        (date: ['8/29/2023 12:00 PM'], result: 'M/d/yyyy HH:mm a', locale: 'en'),
+        (date: ['29/8/2023 12:00 PM'], result: 'd/M/yyyy HH:mm a', locale: 'en'),
+        (date: ['6/20/23', '8/12/23'], result: 'M/d/yy', locale: 'en'),
+        (date: ['6/2/23', '18/12/23'], result: 'd/M/yy', locale: 'en'),
+        (date: ['2023-06-02', '2023-12-18'], result: 'yyyy-M-d', locale: 'en'),
+        (date: ["1/ 1' 0", "1/ 1' 12"], result: "M/ d'' yy", locale: 'en'),
+        (date: ['8/13/2023 23:00'], result: 'M/d/yyyy HH:mm', locale: 'en'),
+        (date: ['8/13/2023 23:00'], result: 'd/M/yyyy HH:mm', locale: 'de'),
+      ];
+
+      for (var v in testCases) {
+        test('$v', () {
+          expect(object.detectFormat(v.date, v.locale), v.result);
+        });
+      }
+    });
+  });
+}
