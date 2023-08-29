ad995ee87338af101b73c4301e778ce8a43926b0
Viachaslau Lyskouski <deus@creativity.by>
2023-08-29 19:48:43 +0200

    [#189] [NF] Import QIF/OFX/HBCI Files. Implement date recognition

--- lib/_mixins/date_format_mixin.dart
+++ lib/_mixins/date_format_mixin.dart
@@ -4,6 +4,13 @@
 import 'package:app_finance/_classes/herald/app_locale.dart';
 import 'package:app_finance/widgets/_wrappers/table_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class _Assertion {
+  int min = 0;
+  int max = 0;
+  int length = 0;
+}
 
 mixin DateFormatMixin {
   Widget showDateTimeHelper(BuildContext context) {
@@ -28,13 +35,87 @@ mixin DateFormatMixin {
     );
   }
 
-  String detectFormat(List<String> data, String locale) {
-    final format = List<String>.filled(splitDateTime(data.first).length, '');
-    return format.join('');
+  String detectFormat(List<String> data, String locale, [List<String>? ampm]) {
+    ampm ??= [AppLocale.labels.dtAm, AppLocale.labels.dtPm];
+    List<String?> format = [];
+    Map<int, _Assertion> check = {};
+    for (int i = 0; i < data.length; i++) {
+      final value = splitDateTime(data[i]);
+      if (format.length < value.length) {
+        format.addAll(List<String?>.filled(value.length - format.length, null));
+      }
+      for (int j = 0; j < value.length; j++) {
+        final nm = int.tryParse(value[j]);
+        if (value[j] == "'") {
+          format[j] = "''";
+        } else if (value[j].contains(RegExp(r'[,.\\\/\:\|; -]'))) {
+          format[j] = value[j];
+        } else if (nm != null) {
+          check[j] ??= _Assertion()
+            ..min = nm
+            ..max = nm
+            ..length = value[j].length;
+          check[j]!.min = check[j]!.min > nm ? nm : check[j]!.min;
+          check[j]!.max = check[j]!.max < nm ? nm : check[j]!.max;
+          check[j]!.length = check[j]!.length < value[j].length ? value[j].length : check[j]!.length;
+        } else if (value.length > j + 1 && value[j + 1] == ',') {
+          format[j] = 'E';
+        } else if (ampm.contains(value[j].toLowerCase())) {
+          format[j] = 'a';
+        } else {
+          format[j] = "'${value[j].replaceAll("'", "''")}'";
+        }
+      }
+    }
+    return _fillDates(format, check, locale).join('');
+  }
+
+  List<String> _fillDates(List<String?> format, Map<int, _Assertion> check, String locale) {
+    bool isMonth = _isMonthFirst(locale);
+    final timeDiv = format.where((v) => v == ':').length;
+    final time = ['HH', 'mm', if (timeDiv > 1) 'ss'];
+    final dmy = ['y', 'M', 'd'];
+    final idxFist = format.indexOf(null);
+    final firstDay = check[idxFist]?.max ?? 0;
+    List<String> dates = isMonth && firstDay <= 12 ? ['M', 'd'] : ['d', 'M'];
+    if (firstDay > 31 || check[idxFist]?.min == 0) {
+      final idxSecond = format.indexOf(null, idxFist + 1);
+      if (isMonth && (check[idxSecond]?.max ?? 0) > 12) {
+        dates = dates.reversed.toList();
+      }
+      dates.insert(0, 'y');
+    } else {
+      dates.add('y');
+    }
+    final max = format.length - 1;
+    for (int i = max; i >= 0; i--) {
+      if (format[i] != null) {
+        continue;
+      }
+      if (time.isNotEmpty && (i > 1 && format[i - 1] == ':' || i + 1 < max && format[i + 1] == ':')) {
+        format[i] = time.removeLast();
+      } else if (dmy.isNotEmpty && (i > 1 && format[i - 1] == '-' || i + 1 < max && format[i + 1] == '-')) {
+        format[i] = dmy.removeLast();
+      } else if (dates.isNotEmpty && check[i] != null) {
+        format[i] = dates.removeLast();
+      } else {
+        format[i] = '?';
+      }
+      if (format[i] == 'y') {
+        format[i] = List<String>.filled(check[i]?.length ?? 1, 'y').join('');
+      }
+    }
+    return format.cast();
+  }
+
+  bool _isMonthFirst(String locale) {
+    DateFormat dateFormat = DateFormat.yMd(locale);
+    List<String> formattedDate = dateFormat.format(DateTime(2023, 10, 20)).split('/');
+    return formattedDate.indexOf('10') < formattedDate.indexOf('20');
   }
 
   List<String> splitDateTime(String value) {
-    RegExp regex = RegExp(r'\d+|[,.\\\/\:\|;]| |\w+');
+    RegExp regex = RegExp(r"\d+|[,.\\\/\:\|;-]|'| |\w+");
     return regex.allMatches(value).map((match) => match.group(0)!).toList();
   }
 }
--- test/unit/_mixins/date_format_mixin_test.dart
+++ test/unit/_mixins/date_format_mixin_test.dart
@@ -27,20 +27,21 @@ void main() {
 
     group('detectFormat', () {
       final testCases = [
-        (date: ['Sun, 8/13/2023'], result: 'E, M/d/yyyy', locale: 'en'),
-        (date: ['8/29/2023 12:00 PM'], result: 'M/d/yyyy HH:mm a', locale: 'en'),
-        (date: ['29/8/2023 12:00 PM'], result: 'd/M/yyyy HH:mm a', locale: 'en'),
-        (date: ['6/20/23', '8/12/23'], result: 'M/d/yy', locale: 'en'),
-        (date: ['6/2/23', '18/12/23'], result: 'd/M/yy', locale: 'en'),
-        (date: ['2023-06-02', '2023-12-18'], result: 'yyyy-M-d', locale: 'en'),
-        (date: ["1/ 1' 0", "1/ 1' 12"], result: "M/ d'' yy", locale: 'en'),
-        (date: ['8/13/2023 23:00'], result: 'M/d/yyyy HH:mm', locale: 'en'),
-        (date: ['8/13/2023 23:00'], result: 'd/M/yyyy HH:mm', locale: 'de'),
+        (date: ['Sun, 8/13/2023'], result: 'E, M/d/yyyy', locale: 'en_US'),
+        (date: ['8/29/2023 12:00 PM'], result: 'M/d/yyyy HH:mm a', locale: 'en_US'),
+        (date: ['29/8/2023 12:00 PM'], result: 'd/M/yyyy HH:mm a', locale: 'en_US'),
+        (date: ['6/20/23', '8/12/23'], result: 'M/d/yy', locale: 'en_US'),
+        (date: ['6/2/23', '18/12/23'], result: 'd/M/yy', locale: 'en_US'),
+        (date: ['2023-06-02', '2023-12-18'], result: 'yyyy-M-d', locale: 'en_US'),
+        (date: ["1/ 1' 0", "1/ 1' 12"], result: "M/ d'' yy", locale: 'en_US'),
+        (date: ['8/13/2023 23:00'], result: 'M/d/yyyy HH:mm', locale: 'en_US'),
+        // (date: ['8/13/2023 23:00'], result: 'd/M/yyyy HH:mm', locale: 'de'), // Locale has not been initialized
+        (date: ['24/12/2023 23:00:12'], result: 'd/M/yyyy HH:mm:ss', locale: 'en_US'),
       ];
 
       for (var v in testCases) {
-        test('$v', () {
-          expect(object.detectFormat(v.date, v.locale), v.result);
+        test('$v', () async {
+          expect(object.detectFormat(v.date, v.locale, ['am', 'pm']), v.result);
         });
       }
     });
