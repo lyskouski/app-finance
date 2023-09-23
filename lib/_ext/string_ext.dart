56a2c9a1ab71c612c5b47cee03a3ff521269d4ec
Viachaslau Lyskouski <deus@creativity.by>
2023-09-23 10:28:58 +0200

    [#258] [NF] Home Page Customization. Extend conversion

--- lib/_ext/string_ext.dart
+++ lib/_ext/string_ext.dart
@@ -1,6 +1,8 @@
 // Copyright 2023 The terCAD team. All rights reserved.
 // Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
 
+import 'dart:convert';
+
 import 'package:app_finance/_configs/custom_text_theme.dart';
 import 'package:app_finance/_ext/build_context_ext.dart';
 import 'package:app_finance/widgets/wrapper/text_wrapper.dart';
@@ -26,6 +28,27 @@ extension MapExt on String {
       }
       final key = _asType<T>(parts[0].trim());
       result[key] = _asType<K>(parts[1].trim());
+    }
+    return result;
+  }
+
+  String _wrap() {
+    if (contains('{')) {
+      RegExp pattern = RegExp(r"(\w+):\s*([\w\.\-]+)");
+      return replaceAllMapped(pattern, (match) {
+        String key = match.group(1)!;
+        String value = match.group(2)!;
+        return '"$key": ${num.tryParse(value) ?? '"$value"'}';
+      });
+    }
+    return this;
+  }
+
+  List<T> toList<T>() {
+    final data = length > 0 ? json.decode(_wrap()) : [];
+    final List<T> result = [];
+    for (final value in data) {
+      result.add(value);
     }
     return result;
   }
