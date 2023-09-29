d0616b98534fe888d65cb0fab9a9a7f42298a547
lyskouski <lyskouski@users.noreply.github.com>
2023-09-29 10:37:57 +0000

    deploy: bb90ba2727cb6582d130a216668ebaa5b2ccc1dd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-512.png": "d0a7ddaee9f1d4b7ed2a95ba8e5e50e6",
 "icons/Icon-192.png": "7d1365a199f68916503a15ecc4c8f5a0",
 "icons/Icon-maskable-512.png": "7d1365a199f68916503a15ecc4c8f5a0",
-"index.html": "d6049b3114e7f2e0e54c724ef2c340df",
-"/": "d6049b3114e7f2e0e54c724ef2c340df",
-"main.dart.js": "0b4aa74e514466818b76a0c7a7b5bdcc",
+"index.html": "3e2fc4c325658cbd9fdd4f1f5eeeb25c",
+"/": "3e2fc4c325658cbd9fdd4f1f5eeeb25c",
+"main.dart.js": "30fd707657746d4df718325dae9843f9",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "6eb0f694702967bb15c471e8c8d3cb12",
 "manifest.json": "45a3f1e8a23817e9bf752339034f6320"};
