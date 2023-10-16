5074bd804d399fde089d1823b02b5863586177ec
Viachaslau Lyskouski <deus@creativity.by>
2023-10-16 17:12:11 +0200

    [#100] [NF] Apple Subscription. Add intermediate state

--- lib/pages/subscription/widgets/purchase_widget.dart
+++ lib/pages/subscription/widgets/purchase_widget.dart
@@ -6,6 +6,7 @@ import 'package:app_finance/_classes/herald/app_locale.dart';
 import 'package:app_finance/_configs/custom_text_theme.dart';
 import 'package:app_finance/_configs/theme_helper.dart';
 import 'package:app_finance/_ext/build_context_ext.dart';
+import 'package:app_finance/widgets/generic/loading_widget.dart';
 import 'package:app_finance/widgets/generic/notification_bar.dart';
 import 'package:app_finance/widgets/wrapper/row_widget.dart';
 import 'package:app_finance/widgets/wrapper/text_wrapper.dart';
@@ -14,7 +15,7 @@ import 'package:flutter_currency_picker/flutter_currency_picker.dart';
 import 'package:flutter_grid_layout/flutter_grid_layout.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 
-class PurchaseWidget extends StatelessWidget {
+class PurchaseWidget extends StatefulWidget {
   final String path;
   final ProductDetails? product;
   final List<PurchaseDetails>? purchase;
@@ -24,17 +25,32 @@ class PurchaseWidget extends StatelessWidget {
   const PurchaseWidget(this.path, {super.key, required this.title, this.product, this.purchase, this.size = 150});
 
   @override
+  PurchaseWidgetState createState() => PurchaseWidgetState();
+}
+
+class PurchaseWidgetState extends State<PurchaseWidget> {
+  bool isLoading = false;
+
+  Future<void> _purchase() async {
+    if (widget.product == null) {
+      NotificationBar.showSnackBar(context, AppLocale.labels.subscriptionInactive, true);
+    } else {
+      setState(() => isLoading = true);
+      await PurchaseController.buy(widget.product!);
+      setState(() => isLoading = false);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final indent = ThemeHelper.getIndent(0.5);
-    final cost = product?.rawPrice.toCurrency(code: product?.currencyCode, withPattern: false) ?? '---';
+    final cost = widget.product?.rawPrice.toCurrency(code: widget.product?.currencyCode, withPattern: false) ?? '---';
     final costWidth = ThemeHelper.getTextWidth(Text(cost));
     return InkWell(
-      onTap: () => product != null
-          ? PurchaseController.buy(product!)
-          : NotificationBar.showSnackBar(context, AppLocale.labels.subscriptionInactive, true),
+      onTap: _purchase,
       child: Container(
-        width: size,
-        height: size,
+        width: widget.size,
+        height: widget.size,
         margin: EdgeInsets.all(indent),
         decoration: BoxDecoration(
           color: context.colorScheme.background,
@@ -48,61 +64,63 @@ class PurchaseWidget extends StatelessWidget {
             ),
           ],
         ),
-        child: GridContainer(
-          rows: const [40, null, 40],
-          columns: const [40, null, 34],
-          children: [
-            GridItem(
-              start: const Size(0, 0),
-              end: const Size(4, 4),
-              child: SizedBox(
-                child: ClipPath(
-                  clipper: _TopRightClipper(),
-                  child: Image.asset(path),
-                ),
+        child: isLoading
+            ? LoadingWidget(isLoading: isLoading, size: Size(widget.size, widget.size))
+            : GridContainer(
+                rows: const [40, null, 40],
+                columns: const [40, null, 34],
+                children: [
+                  GridItem(
+                    start: const Size(0, 0),
+                    end: const Size(4, 4),
+                    child: SizedBox(
+                      child: ClipPath(
+                        clipper: _TopRightClipper(),
+                        child: Image.asset(widget.path),
+                      ),
+                    ),
+                  ),
+                  GridItem(
+                    start: const Size(2, 0),
+                    end: const Size(3, 1),
+                    child: Icon(
+                      switch (widget.purchase?.last.status) {
+                        PurchaseStatus.purchased => Icons.check,
+                        PurchaseStatus.pending => Icons.pending,
+                        _ => Icons.add
+                      },
+                      color: context.colorScheme.secondary.withOpacity(0.3),
+                    ),
+                  ),
+                  GridItem(
+                    start: const Size(0, 2),
+                    end: const Size(3, 3),
+                    order: 2,
+                    child: Container(
+                        color: context.colorScheme.background.withOpacity(0.9),
+                        padding: EdgeInsets.all(indent),
+                        child: RowWidget(
+                          chunk: [null, costWidth],
+                          indent: indent,
+                          alignment: MainAxisAlignment.spaceBetween,
+                          maxWidth: widget.size - indent * 2,
+                          children: [
+                            [
+                              Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: TextWrapper(widget.title, maxLines: 2, style: context.textTheme.bodySmall)),
+                            ],
+                            [
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: TextWrapper(cost, style: context.textTheme.numberMedium),
+                              ),
+                            ],
+                          ],
+                        )),
+                  ),
+                ],
               ),
-            ),
-            GridItem(
-              start: const Size(2, 0),
-              end: const Size(3, 1),
-              child: Icon(
-                switch (purchase?.last.status) {
-                  PurchaseStatus.purchased => Icons.check,
-                  PurchaseStatus.pending => Icons.pending,
-                  _ => Icons.add
-                },
-                color: context.colorScheme.secondary.withOpacity(0.3),
-              ),
-            ),
-            GridItem(
-              start: const Size(0, 2),
-              end: const Size(3, 3),
-              order: 2,
-              child: Container(
-                  color: context.colorScheme.background.withOpacity(0.9),
-                  padding: EdgeInsets.all(indent),
-                  child: RowWidget(
-                    chunk: [null, costWidth],
-                    indent: indent,
-                    alignment: MainAxisAlignment.spaceBetween,
-                    maxWidth: size - indent * 2,
-                    children: [
-                      [
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: TextWrapper(title, maxLines: 2, style: context.textTheme.bodySmall)),
-                      ],
-                      [
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: TextWrapper(cost, style: context.textTheme.numberMedium),
-                        ),
-                      ],
-                    ],
-                  )),
-            ),
-          ],
-        ),
       ),
     );
   }
