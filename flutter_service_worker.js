52c2ad7453ca49baedcdedc858ea8e8a8caae438
lyskouski <lyskouski@users.noreply.github.com>
2023-10-22 21:53:15 +0000

    deploy: 5cff355c80966c676c06b9fbfd2fa03d7817aead

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "8b1e874557a01863719740c9008b254a",
-"/": "8b1e874557a01863719740c9008b254a",
+const RESOURCES = {"index.html": "4cff306d33778d2af16d26ba1bc66c46",
+"/": "4cff306d33778d2af16d26ba1bc66c46",
 "version.json": "a7ea6b983c85cb7edcd843ff51b32114",
 "favicon.png": "6eb0f694702967bb15c471e8c8d3cb12",
 "assets/AssetManifest.bin": "91e17df25bf4794a05fe6549fdcd8608",
