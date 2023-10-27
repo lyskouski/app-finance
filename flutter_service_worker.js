d289332806d516d17adf2a720a71e58c2288983d
lyskouski <lyskouski@users.noreply.github.com>
2023-10-27 11:32:03 +0000

    deploy: ea98a491d9395752a6d88b6631336bdfeebbcf6f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "c9caf379dc7548a2bb4fc10eeaa4e786",
+const RESOURCES = {"main.dart.js": "60c7609e06f1449ba1b3ec3fbd3411a2",
 "assets/AssetManifest.json": "dfe5f91fc8c008125d6fdc00516ee833",
 "assets/assets/l10n/help_metrics_bill_be.md": "5e410ae927d87136d980a658ca8490b7",
 "assets/assets/l10n/help_metrics_bill_en.md": "c813c89ce576596b753c04451bbb3500",
@@ -32,8 +32,8 @@ const RESOURCES = {"main.dart.js": "c9caf379dc7548a2bb4fc10eeaa4e786",
 "assets/NOTICES": "f89c7f540af4e465b4c70200fa911c56",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
 "manifest.json": "45a3f1e8a23817e9bf752339034f6320",
-"index.html": "cca9f1b045e156a4ae745c95553f6122",
-"/": "cca9f1b045e156a4ae745c95553f6122",
+"index.html": "185210ca813cf1588015ff39f125b521",
+"/": "185210ca813cf1588015ff39f125b521",
 "icons/Icon-maskable-512.png": "7d1365a199f68916503a15ecc4c8f5a0",
 "icons/Icon-512.png": "d0a7ddaee9f1d4b7ed2a95ba8e5e50e6",
 "icons/Icon-192.png": "7d1365a199f68916503a15ecc4c8f5a0",
