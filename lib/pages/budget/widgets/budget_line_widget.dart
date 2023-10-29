6adf799887ab382ddded3c78d022e6278c173a41
Viachaslau Lyskouski <deus@creativity.by>
2023-10-29 10:32:31 +0100

    [#358] [NF] Edit Invoices. Enable swipe for budgets

--- lib/pages/budget/widgets/budget_line_widget.dart
+++ lib/pages/budget/widgets/budget_line_widget.dart
@@ -6,6 +6,7 @@ import 'package:app_finance/_configs/custom_text_theme.dart';
 import 'package:app_finance/_configs/theme_helper.dart';
 import 'package:app_finance/_ext/build_context_ext.dart';
 import 'package:app_finance/charts/bar_horizontal_single.dart';
+import 'package:app_finance/widgets/generic/base_swipe_widget.dart';
 import 'package:app_finance/widgets/wrapper/number_wrapper.dart';
 import 'package:app_finance/widgets/wrapper/row_widget.dart';
 import 'package:app_finance/widgets/wrapper/tap_widget.dart';
@@ -48,57 +49,61 @@ class BudgetLineWidget extends StatelessWidget {
     final indent = ThemeHelper.getIndent();
     final textTheme = context.textTheme;
     const iconSize = 18.0;
-    return TapWidget(
-      tooltip: '',
-      route: RouteSettings(name: route, arguments: {routeArguments.uuid: uuid}),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          RowWidget(
-            chunk: [
-              iconSize,
-              null,
-              ThemeHelper.getTextWidth(Text(
-                details,
-                maxLines: 1,
-                style: textTheme.numberMedium,
-                overflow: TextOverflow.ellipsis,
-              )),
-            ],
-            indent: indent,
-            maxWidth: width + indent,
-            children: [
-              [
-                Icon(icon, color: color, size: iconSize),
+    return BaseSwipeWidget(
+      routePath: AppRoute.budgetEditRoute,
+      uuid: uuid,
+      child: TapWidget(
+        tooltip: '',
+        route: RouteSettings(name: route, arguments: {routeArguments.uuid: uuid}),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RowWidget(
+              chunk: [
+                iconSize,
+                null,
+                ThemeHelper.getTextWidth(Text(
+                  details,
+                  maxLines: 1,
+                  style: textTheme.numberMedium,
+                  overflow: TextOverflow.ellipsis,
+                )),
               ],
-              if (description != '')
+              indent: indent,
+              maxWidth: width + indent,
+              children: [
                 [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextWrapper(title, style: textTheme.bodyMedium),
-                      TextWrapper(description, style: textTheme.numberSmall),
-                    ],
-                  ),
-                ]
-              else
-                [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, indent - 1, 0, indent - 1),
-                    child: TextWrapper(title, style: textTheme.bodyMedium),
-                  ),
+                  Icon(icon, color: color, size: iconSize),
                 ],
-              [
-                NumberWidget(details, colorScheme: context.colorScheme, style: textTheme.numberMedium),
-              ]
+                if (description != '')
+                  [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextWrapper(title, style: textTheme.bodyMedium),
+                        TextWrapper(description, style: textTheme.numberSmall),
+                      ],
+                    ),
+                  ]
+                else
+                  [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, indent - 1, 0, indent - 1),
+                      child: TextWrapper(title, style: textTheme.bodyMedium),
+                    ),
+                  ],
+                [
+                  NumberWidget(details, colorScheme: context.colorScheme, style: textTheme.numberMedium),
+                ]
+              ],
+            ),
+            if (showDivider) ...[
+              ThemeHelper.hIndent05,
+              BarHorizontalSingle(value: progress, width: width, color: color),
+              ThemeHelper.hIndent05,
             ],
-          ),
-          if (showDivider) ...[
-            ThemeHelper.hIndent05,
-            BarHorizontalSingle(value: progress, width: width, color: color),
-            ThemeHelper.hIndent05,
           ],
-        ],
+        ),
       ),
     );
   }
