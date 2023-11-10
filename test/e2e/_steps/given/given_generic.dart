f7d1d06952d744918150f209c8d9ae210edbd2cc
Viachaslau Lyskouski <deus@creativity.by>
2023-11-10 21:55:15 +0100

    [#393] [RF] Generators Separation. Fix nested structures

--- test/e2e/_steps/given/given_generic.dart
+++ test/e2e/_steps/given/given_generic.dart
@@ -3,6 +3,22 @@
 
 import 'package:flutter_gherkin_generator/gen/generate_gherkin_resources.dart';
 import 'package:flutter_gherkin_wrapper/flutter_gherkin_wrapper.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 @GenerateGherkinResources(['../../'])
-class Generics extends GivenGeneric {}
+class Generics extends GivenGeneric {
+  @override
+  RegExp get pattern => RegExp('%step%');
+
+  @override
+  // Execute sub-step
+  Future<void> executeStep() async {
+    final reporter = FileReporter();
+    final step = await FileReader().getFromString('''%feature%''', reporter);
+    final result = await FileRunner(FileRunner.tester, reporter).run(step);
+    if (!result) {
+      reporter.publish();
+    }
+    expectSync(result, true);
+  }
+}
