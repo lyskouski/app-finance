d6a2ad4d60d84f5106678655bf0b1f5425897528
lyskouski <lyskouski@users.noreply.github.com>
2023-11-23 08:22:40 +0000

    deploy: 9efa9b880c81b01a74e3ca487918e28911e5d384

--- flutter.js
+++ flutter.js
@@ -144,7 +144,7 @@ _flutter.loader = null;
 
       const serviceWorkerActivation = navigator.serviceWorker
         .register(url)
-        .then((serviceWorkerRegistration) => this._getNewServiceWorker(serviceWorkerRegistration, serviceWorkerVersion))
+        .then(this._getNewServiceWorker)
         .then(this._waitForServiceWorkerActivation);
 
       // Timeout race promise
@@ -156,47 +156,53 @@ _flutter.loader = null;
     }
 
     /**
-     * Returns the latest service worker for the given `serviceWorkerRegistration`.
+     * Returns the latest service worker for the given `serviceWorkerRegistrationPromise`.
      *
      * This might return the current service worker, if there's no new service worker
      * awaiting to be installed/updated.
      *
-     * @param {ServiceWorkerRegistration} serviceWorkerRegistration
-     * @param {String} serviceWorkerVersion
+     * @param {Promise<ServiceWorkerRegistration>} serviceWorkerRegistrationPromise
      * @returns {Promise<ServiceWorker>}
      */
-    async _getNewServiceWorker(serviceWorkerRegistration, serviceWorkerVersion) {
-      if (!serviceWorkerRegistration.active && (serviceWorkerRegistration.installing || serviceWorkerRegistration.waiting)) {
+    async _getNewServiceWorker(serviceWorkerRegistrationPromise) {
+      const reg = await serviceWorkerRegistrationPromise;
+
+      if (!reg.active && (reg.installing || reg.waiting)) {
         // No active web worker and we have installed or are installing
         // one for the first time. Simply wait for it to activate.
         console.debug("Installing/Activating first service worker.");
-        return serviceWorkerRegistration.installing || serviceWorkerRegistration.waiting;
-      } else if (!serviceWorkerRegistration.active.scriptURL.endsWith(serviceWorkerVersion)) {
+        return reg.installing || reg.waiting;
+      } else if (!reg.active.scriptURL.endsWith(serviceWorkerVersion)) {
         // When the app updates the serviceWorkerVersion changes, so we
         // need to ask the service worker to update.
-        const newRegistration = await serviceWorkerRegistration.update();
-        console.debug("Updating service worker.");
-        return newRegistration.installing || newRegistration.waiting || newRegistration.active;
+        return reg.update().then((newReg) => {
+          console.debug("Updating service worker.");
+          return newReg.installing || newReg.waiting || newReg.active;
+        });
       } else {
         console.debug("Loading from existing service worker.");
-        return serviceWorkerRegistration.active;
+        return reg.active;
       }
     }
 
     /**
-     * Returns a Promise that resolves when the `serviceWorker` changes its
+     * Returns a Promise that resolves when the `latestServiceWorker` changes its
      * state to "activated".
      *
-     * @param {ServiceWorker} serviceWorker
+     * @param {Promise<ServiceWorker>} latestServiceWorkerPromise
      * @returns {Promise<void>}
      */
-    async _waitForServiceWorkerActivation(serviceWorker) {
+    async _waitForServiceWorkerActivation(latestServiceWorkerPromise) {
+      const serviceWorker = await latestServiceWorkerPromise;
+
       if (!serviceWorker || serviceWorker.state == "activated") {
         if (!serviceWorker) {
-          throw new Error("Cannot activate a null service worker!");
+          return Promise.reject(
+            new Error("Cannot activate a null service worker!")
+          );
         } else {
           console.debug("Service worker already active.");
-          return;
+          return Promise.resolve();
         }
       }
       return new Promise((resolve, _) => {
