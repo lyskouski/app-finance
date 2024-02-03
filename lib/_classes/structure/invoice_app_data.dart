f2cbd70ce14c51c29c55d8775e3ba6a0d9d1fcad
Viachaslau Lyskouski <deus@creativity.by>
2024-02-03 20:26:24 +0100

    [#469] [CI] Transactions aren't saving. Drop import

--- lib/_classes/structure/invoice_app_data.dart
+++ lib/_classes/structure/invoice_app_data.dart
@@ -4,8 +4,6 @@
 import 'package:app_finance/_classes/herald/app_locale.dart';
 import 'package:app_finance/_classes/storage/app_data.dart';
 import 'package:app_finance/_classes/structure/abstract_app_data.dart';
-// ignore: unused_import
-import 'package:app_finance/_ext/int_ext.dart';
 import 'package:app_finance/_mixins/storage_mixin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_currency_picker/flutter_currency_picker.dart';
