56a00c2268f8661fe08f4e4af59598b054b24135
Viachaslau Lyskouski <deus@creativity.by>
2024-09-12 16:12:35 +0200

    [#217] [RF] Revise Architecture. Shift component widgets

--- lib/components/widgets/budget_widget.dart
+++ lib/components/widgets/budget_widget.dart
@@ -6,7 +6,7 @@ import 'package:app_finance/_classes/structure/navigation/app_route.dart';
 import 'package:app_finance/_classes/structure/budget_app_data.dart';
 import 'package:app_finance/_configs/theme_helper.dart';
 import 'package:app_finance/design/generic/base_swipe_widget.dart';
-import 'package:app_finance/pages/home/widgets/account_widget.dart';
+import 'package:app_finance/components/widgets/account_widget.dart';
 import 'package:app_finance/design/generic/base_group_widget.dart';
 import 'package:app_finance/design/generic/base_line_widget.dart';
 import 'package:flutter/material.dart';
--- lib/pages/budget/widgets/budget_widget.dart
+++ lib/pages/budget/widgets/budget_widget.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
 
 import 'package:app_finance/pages/budget/widgets/budget_line_widget.dart';
-import 'package:app_finance/pages/home/widgets/budget_widget.dart' as home;
+import 'package:app_finance/components/widgets/budget_widget.dart' as home;
 import 'package:flutter/material.dart';
 
 class BudgetWidget extends home.BudgetWidget {
