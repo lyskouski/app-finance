63575988e6a8f3eeb906f1c29eac8354db5caca0
Viachaslau Lyskouski <deus@creativity.by>
2024-10-04 12:31:51 +0200

    [#498] [CR] LocalStorage limitations. Switch to indexedDB for Web

--- lib/_classes/storage/transaction_log/abstract_storage_web.dart
+++ lib/_classes/storage/transaction_log/abstract_storage_web.dart
@@ -4,9 +4,22 @@
 import 'package:app_finance/_classes/storage/app_preferences.dart';
 import 'package:app_finance/_classes/storage/transaction_log/interface_storage.dart';
 import 'package:app_finance/_ext/int_ext.dart';
+import 'package:idb_shim/idb_browser.dart';
 
 abstract class AbstractStorage implements InterfaceStorage {
   static int increment = 0;
+  static const String storeName = 'records';
+  static Database? db;
+
+  static Future<void> _initIndexedDB() async {
+    IdbFactory? idbFactory = getIdbFactory();
+    if (idbFactory == null) {
+      return;
+    }
+    db = await idbFactory.open('fingrom.db', version: 1, onUpgradeNeeded: (VersionChangeEvent event) {
+      event.database.createObjectStore(storeName, keyPath: 'id');
+    });
+  }
 
   static Future<String> getSize() async {
     int size = increment * 256;
@@ -14,16 +27,20 @@ abstract class AbstractStorage implements InterfaceStorage {
   }
 
   static void saveRaw(String line) {
-    AppPreferences.set('log$increment', line);
+    if (db != null) {
+      var store = db!.transaction(storeName, 'readwrite').objectStore(storeName);
+      store.put({'id': 'log$increment', 'line': line});
+    } else {
+      AppPreferences.set('log$increment', line);
+    }
     increment++;
   }
 
-  static Stream<String> read() async* {
-    increment = 0;
+  static Stream<String> readRaw(Function callback) async* {
     int attempts = 0;
     do {
       int i = increment + attempts;
-      var line = AppPreferences.get('log$i');
+      String? line = await callback(i);
       if (line == null) {
         attempts++;
       } else {
@@ -34,9 +51,26 @@ abstract class AbstractStorage implements InterfaceStorage {
     } while (attempts < 10);
   }
 
+  static Stream<String> read() async* {
+    increment = 0;
+    await _initIndexedDB();
+    await for (var line in readRaw((i) => AppPreferences.get('log$i'))) {
+      yield line;
+    }
+    if (db != null) {
+      var store = db!.transaction(storeName, 'readonly').objectStore(storeName);
+      await for (var line in readRaw((i) async => ((await store.getObject('log$i')) as Map?)?['line'])) {
+        yield line;
+      }
+    }
+  }
+
   static void clear() {
     while (increment > 0) {
       AppPreferences.clear('log$increment');
+      if (db != null) {
+        db!.transaction(storeName, 'readwrite').objectStore(storeName).delete('log$increment');
+      }
       increment--;
     }
   }
