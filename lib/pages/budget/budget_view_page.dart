46bdf5fbdbdd1ab88d060bafa7bad14088402c73
Viachaslau Lyskouski <deus@creativity.by>
2024-10-22 10:45:26 +0200

    [#505] [NF] Calendar Start Day. Fix summary tables

--- lib/pages/budget/budget_view_page.dart
+++ lib/pages/budget/budget_view_page.dart
@@ -135,7 +135,7 @@ class BudgetViewPageState extends AbstractPageState<BudgetViewPage> with TickerP
           break;
         }
         iteration++;
-        curr = DateTime(now.year, now.month - iteration).getStartingDay(startingDay);
+        curr = DateTime(now.year, now.month - iteration, now.day).getStartingDay(startingDay);
         summary.details = 0.0;
         summary.color = null;
         summary.title = DateFormat.MMMM().format(curr);
