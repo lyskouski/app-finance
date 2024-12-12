81571189ac9262f6db2fd977200c8efa9e0fd9c6
Viachaslau Lyskouski <deus@creativity.by>
2024-12-12 13:47:26 +0100

    [#532] [CR] App returns to Home instead of previous screen. Fix tests

--- test/e2e/_steps/given/on_defined_page.dart
+++ test/e2e/_steps/given/on_defined_page.dart
@@ -1,7 +1,6 @@
 // Copyright 2023 The terCAD team. All rights reserved.
 // Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_gherkin_wrapper/flutter_gherkin_wrapper.dart';
 import 'package:flutter_test/flutter_test.dart';
 // ignore: depend_on_referenced_packages
@@ -15,25 +14,27 @@ class OnDefinedPage extends Given1<String> {
 
   @override
   Future<void> executeStep(String route) async {
-    // TBD: check flow (unexpected currency selector "force to action")
-    try {
-      final btn = find.byTooltip('Return Back');
-      expectSync(btn, findsOneWidget);
-      await FileRunner.tester.ensureVisible(btn);
-      await FileRunner.tester.tap(btn, warnIfMissed: false);
-      await FileRunner.tester.pumpAndSettle();
-    } catch (_) {
-      // skip
-    }
     ScreenCapture.seize(runtimeType.toString());
-    final ScaffoldState scafState = FileRunner.tester.firstState(find.byType(Scaffold).at(1));
-    scafState.openDrawer();
+    final btn = find.byTooltip('Open main menu');
+    expectSync(btn, findsOneWidget);
+    await FileRunner.tester.ensureVisible(btn);
+    await FileRunner.tester.tap(btn);
     await FileRunner.tester.pumpAndSettle();
-    final header = find.text(route);
-    ScreenCapture.seize(runtimeType.toString());
-    expectSync(header, findsOneWidget);
+
+    ScreenCapture.seize('${runtimeType.toString()}_$route');
+    Finder header = find.text(route);
+    final matchCount = FileRunner.tester.widgetList(header).length;
+    if (matchCount == 1) {
+      // do nothing
+    } else if (matchCount > 1) {
+      header = header.first;
+    } else {
+      fail('No widget found with text "$route"');
+    }
+    await FileRunner.tester.ensureVisible(header);
     await FileRunner.tester.tap(header);
     await FileRunner.tester.pumpAndSettle();
+
     ScreenCapture.seize(runtimeType.toString());
   }
 }
