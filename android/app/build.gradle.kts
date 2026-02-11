19bfe09876f9ad46e25f044673173d5ae36a1a4b
Viachaslau Lyskouski <deus@creativity.by>
2026-02-11 10:32:09 +0100

    [#585] [BF] [Android] Recovery - Save FTL. Resolve compatibility issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     namespace = "com.tercad.fingrom"
     compileSdk = flutter.compileSdkVersion
     // ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
