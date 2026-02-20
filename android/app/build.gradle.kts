790cd9fdd98be4982f2180ed3fdbac23f6d5f77d
Viachaslau Lyskouski <deus@creativity.by>
2026-02-20 13:40:21 +0100

    [#589] [BP] [iOS] [Android] Flutter 3.41 failed to build. Update JVM

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,13 +35,13 @@ android {
     ndkVersion = "28.2.13676358"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
         }
     }
 
