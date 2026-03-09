95591a828a177232c6138a0f0b42ccbb99c81306
Viachaslau Lyskouski <deus@creativity.by>
2026-03-09 12:55:02 +0100

    [#595] [RF] Improve code style. Split state

---
+++ lib/_configs/member_list.dart
@@ -0,0 +1,25 @@
+// Copyright 2026 The terCAD team. All rights reserved.
+// Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
+
+import 'package:app_finance/_classes/herald/app_locale.dart';
+
+final memberList = [
+  (name: 'Viachaslau Lyskouski', badge: [AppLocale.labels.coAuthor]),
+  (name: 'Iryna Shepetovetska', badge: [AppLocale.labels.coTranslator]),
+  (name: 'Sabri Ünal', badge: [AppLocale.labels.coDeveloper]),
+  (name: 'Hazem Abbas', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Raj Jani', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Filip Zatloukal', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Aman Raj Singh Mourya', badge: [AppLocale.labels.coDeveloper]),
+  (name: 'Ivan Alessandro Sala', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Carolina Murazik', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Zbymal', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Hazem', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Andrei Tarasevich', badge: [AppLocale.labels.coConsult]),
+  (name: 'Halina Gulevich', badge: [AppLocale.labels.coConsult]),
+  (name: 'Muhammadyusuf Kurbonov', badge: [AppLocale.labels.coDeveloper]),
+  (name: 'Edivaldo Brito', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Jardeson Márcio', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Paul', badge: [AppLocale.labels.coPromoter]),
+  (name: 'Alexey Alter-Pesotskiy', badge: [AppLocale.labels.coPromoter]),
+];
--- lib/pages/about/widgets/members_tab.dart
+++ lib/pages/about/widgets/members_tab.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
 
 import 'package:app_finance/_classes/herald/app_locale.dart';
+import 'package:app_finance/_configs/member_list.dart';
 import 'package:app_finance/_configs/theme_helper.dart';
 import 'package:app_finance/_mixins/launcher_mixin.dart';
 import 'package:app_finance/design/button/full_sized_button_widget.dart';
@@ -9,27 +10,6 @@ import 'package:app_finance/pages/about/widgets/member_widget.dart';
 import 'package:flutter/material.dart';
 
 class MembersTab extends StatelessWidget with LauncherMixin {
-  static final members = [
-    (name: 'Viachaslau Lyskouski', badge: [AppLocale.labels.coAuthor]),
-    (name: 'Iryna Shepetovetska', badge: [AppLocale.labels.coTranslator]),
-    (name: 'Sabri Ünal', badge: [AppLocale.labels.coDeveloper]),
-    (name: 'Hazem Abbas', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Raj Jani', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Filip Zatloukal', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Aman Raj Singh Mourya', badge: [AppLocale.labels.coDeveloper]),
-    (name: 'Ivan Alessandro Sala', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Carolina Murazik', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Zbymal', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Hazem', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Andrei Tarasevich', badge: [AppLocale.labels.coConsult]),
-    (name: 'Halina Gulevich', badge: [AppLocale.labels.coConsult]),
-    (name: 'Muhammadyusuf Kurbonov', badge: [AppLocale.labels.coDeveloper]),
-    (name: 'Edivaldo Brito', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Jardeson Márcio', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Paul', badge: [AppLocale.labels.coPromoter]),
-    (name: 'Alexey Alter-Pesotskiy', badge: [AppLocale.labels.coPromoter]),
-  ];
-
   const MembersTab({super.key});
 
   @override
@@ -47,7 +27,7 @@ class MembersTab extends StatelessWidget with LauncherMixin {
           childAspectRatio: width,
         ),
         shrinkWrap: true,
-        itemCount: members.length + 1,
+        itemCount: memberList.length + 1,
         itemBuilder: (BuildContext context, int index) => index == 0
             ? Padding(
                 padding: EdgeInsets.only(right: indent, top: indent),
@@ -57,7 +37,7 @@ class MembersTab extends StatelessWidget with LauncherMixin {
                   icon: Icons.add,
                 ),
               )
-            : MemberWidget(members[index - 1]),
+            : MemberWidget(memberList[index - 1]),
       ),
     );
   }
