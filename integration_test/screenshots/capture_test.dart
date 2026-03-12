1540bbc54dc896be8519297e38bf66b58b25cba6
Viachaslau Lyskouski <deus@creativity.by>
2026-03-12 11:03:20 +0100

    [#464] [BP] Screenshots Generator. Skip existance

--- integration_test/screenshots/capture_test.dart
+++ integration_test/screenshots/capture_test.dart
@@ -3,6 +3,7 @@
 
 // flutter test integration_test/screenshots/capture_test.dart
 
+import 'dart:io';
 import 'package:app_finance/_classes/herald/app_design.dart';
 import 'package:app_finance/_classes/herald/app_locale.dart';
 import 'package:app_finance/_classes/storage/app_preferences.dart';
@@ -67,6 +68,10 @@ void main() {
   const stepCsv = String.fromEnvironment('SCREENSHOT_STEPS');
   final stepFilter = stepCsv.split(',').map((v) => v.trim()).where((v) => v.isNotEmpty).toSet();
 
+  // flutter test integration_test/screenshots/capture_test.dart --dart-define=SKIP_EXISTANT=true
+  const skipExistant = String.fromEnvironment('SKIP_EXISTANT');
+  final shouldSkipExisting = skipExistant.toLowerCase() == 'true' || skipExistant == '1';
+
   bool isEnabledStep(String stepId) {
     if (stepFilter.isEmpty) {
       return true;
@@ -78,6 +83,15 @@ void main() {
   for (final locale in locales) {
     final localeCode = locale.toString();
     for (final device in filteredDevices) {
+      // Check if we should skip this locale/device combination
+      if (shouldSkipExisting) {
+        final deviceFolderName = device.name.replaceAll(' ', '_').replaceAll(RegExp(r'[^\w\-_]'), '');
+        final outputPath = Directory('${config.outputDirectory}/$localeCode/$deviceFolderName');
+        if (outputPath.existsSync()) {
+          print('Skipping $localeCode/${device.name} - folder exists with screenshots');
+          continue;
+        }
+      }
       // Home
       if (isEnabledStep('1_home')) {
         _screenshotForDevice('1_home', device, config, localeCode: localeCode);
