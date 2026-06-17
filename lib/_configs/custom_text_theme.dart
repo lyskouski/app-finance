ee264aad6891e7b39eea77507cb9726674a44929
Viachaslau Lyskouski <deus@creativity.by>
2026-06-17 14:41:32 +0200

    [#631] [CR] [FlatHub] Upgrade Gnome Platform. Drop spacing

--- lib/_configs/custom_text_theme.dart
+++ lib/_configs/custom_text_theme.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 extension CustomTextTheme on TextTheme {
-  double letterSpacingOrNone(double letterSpacing) => kIsWeb ? 0.0 : letterSpacing;
-
   TextTheme? withCustom(String paletteType, Brightness brightness) {
     final color = AppColors(paletteType, brightness).palette.secondary;
     return copyWith(
@@ -16,70 +14,60 @@ extension CustomTextTheme on TextTheme {
         fontSize: 32,
         fontWeight: FontWeight.w400,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       titleMedium: GoogleFonts.abel(
         fontSize: 18,
         fontWeight: FontWeight.w400,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       titleSmall: GoogleFonts.abel(
         fontSize: 12,
         fontWeight: FontWeight.w400,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       headlineLarge: GoogleFonts.abel(
         fontSize: 32,
         fontWeight: FontWeight.w400,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       headlineMedium: GoogleFonts.abel(
         fontSize: 14,
         fontWeight: FontWeight.w600,
-        letterSpacing: letterSpacingOrNone(1.2),
+        color: color,
       ),
       headlineSmall: GoogleFonts.abel(
         fontSize: 12,
         fontWeight: FontWeight.w600,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       bodyLarge: GoogleFonts.abel(
         fontSize: 16,
         fontWeight: FontWeight.w600,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       bodyMedium: GoogleFonts.abel(
         fontSize: 14,
         fontWeight: FontWeight.w400,
-        letterSpacing: letterSpacingOrNone(1.2),
+        color: color,
       ),
       bodySmall: GoogleFonts.abel(
         fontSize: 10,
         fontWeight: FontWeight.w600,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.6),
       ),
       labelLarge: GoogleFonts.abel(
         fontSize: 12,
         fontWeight: FontWeight.w600,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       labelMedium: GoogleFonts.abel(
         fontSize: 10,
         fontWeight: FontWeight.w600,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
       labelSmall: GoogleFonts.abel(
         fontSize: 8,
         fontWeight: FontWeight.w600,
         color: color,
-        letterSpacing: letterSpacingOrNone(1.2),
       ),
     );
   }
@@ -88,34 +76,29 @@ extension CustomTextTheme on TextTheme {
         fontSize: 12,
         fontWeight: FontWeight.w400,
         color: titleLarge?.color?.withValues(alpha: 0.4),
-        letterSpacing: letterSpacingOrNone(0.5),
       );
 
   TextStyle get tooltipMedium => GoogleFonts.robotoCondensed(
         fontSize: 16,
         fontWeight: FontWeight.w400,
         color: titleLarge?.color?.withValues(alpha: 0.4),
-        letterSpacing: letterSpacingOrNone(0.5),
       );
 
   TextStyle get numberLarge => GoogleFonts.robotoCondensed(
         fontSize: 32,
         fontWeight: FontWeight.w400,
         color: titleLarge?.color,
-        letterSpacing: letterSpacingOrNone(0.5),
       );
 
   TextStyle get numberMedium => GoogleFonts.robotoCondensed(
         fontSize: 16,
         fontWeight: FontWeight.w400,
         color: titleLarge?.color,
-        letterSpacing: letterSpacingOrNone(0.5),
       );
 
   TextStyle get numberSmall => GoogleFonts.robotoCondensed(
         fontSize: 10,
         fontWeight: FontWeight.w400,
         color: titleLarge?.color,
-        letterSpacing: letterSpacingOrNone(0.5),
       );
 }
