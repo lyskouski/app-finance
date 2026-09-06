70411facb5aec463ba772adc443124db40d4905f
Viachaslau Lyskouski <deus@creativity.by>
2026-09-06 14:33:12 +0200

    [#650] [BF] [Android] Persistent keyboard. Correct identification for test

---
+++ lib/_configs/mobile_platform.dart
@@ -0,0 +1,1 @@
+export 'mobile_platform_stub.dart' if (dart.library.io) 'mobile_platform_io.dart';
---
+++ lib/_configs/mobile_platform_io.dart
@@ -0,0 +1,3 @@
+import 'dart:io';
+
+bool get isMobilePlatform => Platform.isAndroid || Platform.isIOS;
